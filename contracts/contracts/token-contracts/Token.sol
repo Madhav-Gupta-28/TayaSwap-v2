b26695fcbdd9afb92179dfb137b94cf823de798b
Madhav <guptamadhav2828@gmail.com>
2023-08-10 13:22:13 +0530

    add token sol file

---
+++ contracts/contracts/token-contracts/Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.7.0 < 0.9.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token  is ERC20 {
+
+
+    // the max total supply is 1000000 for TayaSwap Tokens
+    uint256 public  maxTotalSupply = 1000000 * 10**18;
+
+    constructor(string memory name  , string memory symbol , uint256 maxSupply  ) ERC20(name, symbol){
+        maxTotalSupply = maxSupply;
+        _mint(msg.sender, maxTotalSupply);
+    }
+
+}
